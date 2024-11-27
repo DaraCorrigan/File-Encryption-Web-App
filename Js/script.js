a6d023ab2631dc4ee4f6c38319a31b11398e3dba
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2024-11-27 09:34:07 +0000

    STABLE AGAIN

--- Js/script.js
+++ Js/script.js
@@ -4,12 +4,19 @@ expand_btn.addEventListener("click", () => {
   document.body.classList.toggle("collapsed");
   const profileDiv = document.querySelector('div#hide-profile');
   const logoutButton = document.querySelector('svg#logout-button');
+  const nav = document.querySelector('nav.navigation-menu');
   if (document.body.classList.contains("collapsed")) {
-    profileDiv.style.display = 'none';
-    logoutButton.style.display = 'none';
+    nav.style.animation = 'navCollapse 0.5s forwards';
+    setTimeout(() => {
+      profileDiv.style.display = 'none';
+      logoutButton.style.display = 'none';
+    }, 200);
   } else {
-    profileDiv.style.display = 'block';
-    logoutButton.style.display = 'block';
+    nav.style.animation = 'navExpand 0.5s forwards';
+    setTimeout(() => {
+      profileDiv.style.display = 'block';
+      logoutButton.style.display = 'block';      
+    }, 70);
   }
 });
 
