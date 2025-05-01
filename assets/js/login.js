ae7dcec76f9e50c23121283ecf917e23358bbec6
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2025-05-01 15:23:21 +0100

    Delete

--- assets/js/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginPopup = document.querySelector('.login-popup-container');
-    const loginButton = document.querySelector('#login-button');
-
-    // Opens the popup when the button is clicked
-    loginButton.addEventListener('click', () => {
-        loginPopup.style.display = 'flex';
-    });
-});
