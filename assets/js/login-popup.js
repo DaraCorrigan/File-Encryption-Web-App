42b8aaa72fbdb76f7a4903aad762f8326703ee47
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2025-05-01 16:22:22 +0100

    Create login-popup.js
    - Controls login / sign up toggle

---
+++ assets/js/login-popup.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.login-popup-container');
+const loginButton = document.querySelector('.toggle-login-button');
+const signupButton = document.querySelector('.toggle-sign-up-button');
+console.log(signupButton);
+
+loginButton.addEventListener('click', () => {
+    container.classList.remove('active');
+});
+
+signupButton.addEventListener('click', () => {
+    container.classList.add('active');
+});
