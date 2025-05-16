7a89d5c552f3f96a1bf02628d4d144788a0f8bf4
DaraCorrigan <148446121+DaraCorrigan@users.noreply.github.com>
2025-05-16 03:26:34 +0100

    Update register.php
    - Added comments
    - Clears session to avoid conflicting user data from previous sessions

--- assets/php/register.php
+++ assets/php/register.php
@@ -18,11 +18,21 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $tripleDesKey = bin2hex(random_bytes(24));
 
     try {
+        // Inserts the user details in the database
         $stmt = $pdo->prepare("INSERT INTO Users (username, password, `AES-256`, `AES-128`, `TripleDES`) VALUES (?, ?, ?, ?, ?)");
         $stmt->execute([$username, $hashedPassword, $aes256Key, $aes128Key, $tripleDesKey]);
 
+        // Retrieves the userID of the new user
+        $userID = $pdo->lastInsertId();
+
+        // Clears the session to prevent any conflicts with the previous users data
+        session_unset();
+        session_destroy();
+        session_start();
+
         $_SESSION['loggedin'] = true;
         $_SESSION['username'] = $username;
+        $_SESSION['userID'] = $userID; // Stores the userID in the session for tools like password manager
 
         echo json_encode(['success' => true, 'message' => 'Registration successful.']);
         exit;
